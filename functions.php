55fce8b56318b57ec942f61096c8b101f1bead37
Nico <jnico.chilla@gmail.com>
2020-08-30 14:33:00 -0700

    testing feTurbulence grain effect

---
+++ functions.php
@@ -0,0 +1,83 @@
+<?php
+function create_post_project() {
+      register_post_type( 'project',
+        array(
+          'labels'       => array(
+          'name'       => __( 'Projects' ),
+          ),
+          'public'       => true,
+          'hierarchical' => true,
+          'has_archive'  => true,
+          'supports'     => array(
+            'title',
+            'editor',
+            'excerpt',
+            'thumbnail',
+          ),
+          'show_in_rest' => true,
+          'taxonomies'   => array(
+            'post_tag',
+            'category',
+          )
+        )
+      );
+      register_taxonomy_for_object_type( 'category', 'project' );
+      register_taxonomy_for_object_type( 'post_tag', 'project' );
+}
+function create_post_concept() {
+      register_post_type( 'concept',
+        array(
+          'labels'       => array(
+          'name'       => __( 'Concepts' ),
+          ),
+          'public'       => true,
+          'hierarchical' => true,
+          'has_archive'  => true,
+          'supports'     => array(
+            'title',
+            'excerpt',
+            'thumbnail',
+          ),
+          'show_in_rest' => true,
+          'taxonomies'   => array(
+            'post_tag',
+            'category',
+          )
+        )
+      );
+      register_taxonomy_for_object_type( 'category', 'concept' );
+      register_taxonomy_for_object_type( 'post_tag', 'concept' );
+}
+function create_post_news() {
+      register_post_type( 'news',
+        array(
+          'labels'       => array(
+          'name'       => __( 'News' ),
+          ),
+          'public'       => true,
+          'hierarchical' => true,
+          'has_archive'  => true,
+          'supports'     => array(
+            'title',
+            'excerpt',
+            'thumbnail',
+          ),
+          'show_in_rest' => true,
+          'taxonomies'   => array(
+            'post_tag',
+            'category',
+          )
+        )
+      );
+      register_taxonomy_for_object_type( 'category', 'news' );
+      register_taxonomy_for_object_type( 'post_tag', 'news' );
+}
+
+
+
+add_theme_support( 'post-thumbnails' );
+add_action( 'init', 'create_post_project' );
+add_action( 'init', 'create_post_concept' );
+add_action( 'init', 'create_post_news' );
+
+ini_set('display_errors',1); error_reporting(E_ALL);
--- home.php
+++ home.php
@@ -1,10 +1,123 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html;" charset="utf-8" >
+    <meta name="viewport" charset="UTF-8" content="width=device-width, initial-scale=1">
     <title>Common Language</title>
+    <!-- styling -->
+    <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/css/clearstyle.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/css/style.css" type="text/css">
+    <!-- typefaces -->
+    <link rel="stylesheet" href="https://use.typekit.net/kfr0yfw.css">
+    <!-- favicon stuff -->
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_bloginfo('template_directory'); ?>/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_bloginfo('template_directory'); ?>/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_bloginfo('template_directory'); ?>/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo get_bloginfo('template_directory'); ?>/favicon/site.webmanifest">
   </head>
-  <body>
-    <p>Greetings, visitor</p>
+  <body class='nosquish'>
+    <div id='scrollcontent'>
+      <div id='nav-dropdown'>
+        <div id="dropper" class='noselect'>
+          <h2>menu</h2>
+          <svg class='dot' viewBox="0 0 10 10" preserveAspectRatio="none">
+            <circle cx="5" cy="5" r="4.7"/>
+          </svg>
+        </div>
+        <div id='sectionlinks'>
+          <div class="h-sectlink noselect">
+            <h2>projects</h2>
+            <svg class='dot' viewBox="0 0 10 10" preserveAspectRatio="none">
+              <circle cx="5" cy="5" r="4.7"/>
+            </svg>
+          </div>
+          <div class="h-sectlink noselect">
+            <h2>profile</h2>
+            <svg class='dot' viewBox="0 0 10 10" preserveAspectRatio="none">
+              <circle cx="5" cy="5" r="4.7"/>
+            </svg>
+          </div>
+          <div class="h-sectlink noselect">
+            <h2>news</h2>
+            <svg class='dot' viewBox="0 0 10 10" preserveAspectRatio="none">
+              <circle cx="5" cy="5" r="4.7"/>
+            </svg>
+          </div>
+          <div class="h-sectlink noselect">
+            <h2>concepts</h2>
+            <svg class='dot' viewBox="0 0 10 10" preserveAspectRatio="none">
+              <circle cx="5" cy="5" r="4.7"/>
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div id='sizereference'></div>
+      <div id='cover'>
+        <div id="gallery">
+          <div class="cardgroup">
+            <div class="imgwrapper">
+              <img src="<?php echo get_bloginfo('template_directory'); ?>/assets/silentgardenwest.png" alt="West Approach">
+            </div>
+            <h4>Silent Garden — Red Hook, Brooklyn</h4>
+          </div>
+
+        </div>
+      </div>
+      <h1>Common Language</h1>
+      <h2>
+        We explore the intersection between Architecture and Music. Both involve the creative use and misuse of rhythm, texture, harmony, pattern, proportion, and dynamics. <br><br>Our fundamental goal is to investigate this relationship through a collaborative making process that constantly re-defines the role of architecture.
+      </h2>
+      <div class="separator">
+          <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="-1 -1 100 102"><path vector-effect='non-scaling-stroke' d="M0,100,20,0,70,100l30-70.07"/></svg>
+      </div>
+      <h2 class='sectionmarker'>News</h2>
+      <h4 class='datemarker'>November 15, 2020</h4>
+      <p class='newstext'>Justin Sherrill was featured on the Parsons School of Constructed Environments blog</p>
+      <h4 class='datemarker'>October 10, 2020</h4>
+      <p class='newstext'>Our mixed-use residential complex design won second place in New Den-cities competition</p>
+
+      <div class="separator">
+        <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="-1 -1 100 102"><path vector-effect='non-scaling-stroke' d="M0,48,30,100,80,0l20,100"/></svg>
+      </div>
+    </div>
+    <div id='fixedcontent' class="page-grid">
+      <div id="horizontal-nav">
+        <div id="sunbox-h">
+          <svg class='sunsvg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5375.26 5375.26"><g class='dotgroup'><g id="rotate"><g id="dots"><circle cx="1497.05" cy="2688.07" r="119.09"/><circle cx="2137.13" cy="2688.07" r="119.09"/><circle cx="2777.21" cy="2688.07" r="119.09"/><circle cx="3417.29" cy="2688.07" r="119.09"/><circle cx="4057.37" cy="2688.07" r="119.09"/><circle cx="4697.44" cy="2688.07" r="119.09"/></g><rect class="svgframe" class="cls-1" x="-0.37" y="-0.37" width="5376" height="5376"/></g></g><g class="sungroup"><circle class="sun" vector-effect='non-scaling-stroke' cx="2687.86" cy="2688.07" r="555.72"/></g>
+          </svg>
+        </div>
+      </div>
+      <div id='vertical-nav'>
+          <div id="sunbox-v" class='viewing nav-item'>
+            <svg class='sunsvg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5375.26 5375.26"><g class='dotgroup'><g id="rotate"><g id="dots"><circle cx="1497.05" cy="2688.07" r="119.09"/><circle cx="2137.13" cy="2688.07" r="119.09"/><circle cx="2777.21" cy="2688.07" r="119.09"/><circle cx="3417.29" cy="2688.07" r="119.09"/><circle cx="4057.37" cy="2688.07" r="119.09"/><circle cx="4697.44" cy="2688.07" r="119.09"/></g><rect class="svgframe" class="cls-1" x="-0.37" y="-0.37" width="5376" height="5376"/></g></g><g class="sungroup"><circle class="sun" vector-effect='non-scaling-stroke' cx="2687.86" cy="2688.07" r="555.72"/></g>
+            </svg>
+          </div>
+          <div id='navwrap'>
+          <div class="section-link nav-item">
+            <svg class='sunsvg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5375.26 5375.26"><g class='dotgroup'><g id="rotate"><g id="dots"><circle cx="1497.05" cy="2688.07" r="119.09"/><circle cx="2137.13" cy="2688.07" r="119.09"/><circle cx="2777.21" cy="2688.07" r="119.09"/><circle cx="3417.29" cy="2688.07" r="119.09"/><circle cx="4057.37" cy="2688.07" r="119.09"/><circle cx="4697.44" cy="2688.07" r="119.09"/></g><rect class="svgframe" class="cls-1" x="-0.37" y="-0.37" width="5376" height="5376"/></g></g><g class="sungroup"><circle class="sun" vector-effect='non-scaling-stroke' cx="2687.86" cy="2688.07" r="555.72"/></g>
+            </svg>
+            <h2>projects</h2>
+          </div>
+          <div class="section-link nav-item">
+            <svg class='sunsvg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5375.26 5375.26"><g class='dotgroup'><g id="rotate"><g id="dots"><circle cx="1497.05" cy="2688.07" r="119.09"/><circle cx="2137.13" cy="2688.07" r="119.09"/><circle cx="2777.21" cy="2688.07" r="119.09"/><circle cx="3417.29" cy="2688.07" r="119.09"/><circle cx="4057.37" cy="2688.07" r="119.09"/><circle cx="4697.44" cy="2688.07" r="119.09"/></g><rect class="svgframe" class="cls-1" x="-0.37" y="-0.37" width="5376" height="5376"/></g></g><g class="sungroup"><circle class="sun" vector-effect='non-scaling-stroke' cx="2687.86" cy="2688.07" r="555.72"/></g>
+            </svg>
+            <h2>profile</h2>
+          </div>
+          <div class="section-link nav-item">
+            <svg class='sunsvg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5375.26 5375.26"><g class='dotgroup'><g id="rotate"><g id="dots"><circle cx="1497.05" cy="2688.07" r="119.09"/><circle cx="2137.13" cy="2688.07" r="119.09"/><circle cx="2777.21" cy="2688.07" r="119.09"/><circle cx="3417.29" cy="2688.07" r="119.09"/><circle cx="4057.37" cy="2688.07" r="119.09"/><circle cx="4697.44" cy="2688.07" r="119.09"/></g><rect class="svgframe" class="cls-1" x="-0.37" y="-0.37" width="5376" height="5376"/></g></g><g class="sungroup"><circle class="sun" vector-effect='non-scaling-stroke' cx="2687.86" cy="2688.07" r="555.72"/></g>
+            </svg>
+            <h2>news</h2>
+          </div>
+          <div class="section-link nav-item">
+            <svg class='sunsvg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5375.26 5375.26"><g class='dotgroup'><g id="rotate"><g id="dots"><circle cx="1497.05" cy="2688.07" r="119.09"/><circle cx="2137.13" cy="2688.07" r="119.09"/><circle cx="2777.21" cy="2688.07" r="119.09"/><circle cx="3417.29" cy="2688.07" r="119.09"/><circle cx="4057.37" cy="2688.07" r="119.09"/><circle cx="4697.44" cy="2688.07" r="119.09"/></g><rect class="svgframe" class="cls-1" x="-0.37" y="-0.37" width="5376" height="5376"/></g></g><g class="sungroup"><circle class="sun" vector-effect='non-scaling-stroke' cx="2687.86" cy="2688.07" r="555.72"/></g>
+            </svg>
+            <h2>concepts</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="<?php echo get_bloginfo('template_directory'); ?>/js/smoothscroll.min.js"></script>
+    <script src="<?php echo get_bloginfo('template_directory'); ?>/js/d3.min.js"></script>
+    <script src="<?php echo get_bloginfo('template_directory'); ?>/js/function.js"></script>
   </body>
 </html>
